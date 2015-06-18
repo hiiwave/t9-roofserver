1007a150d8d4cd64b6ce3d8c3b09ffedd5d6755d
WaveDorm <hiiwave@gmail.com>
2015-06-18 16:51:14 +0800

    .

--- public/monitor.js
+++ public/monitor.js
@@ -13,31 +13,3 @@ socket.on('newPkt', function(pkt) {
   $('#newData').html(str);
   $('#countData').html(1 + parseInt($('#countData').html()));
 });
-
-socket.on('newImg', function(imgpkt) {
-	var b64raw = imgpkt.raw;
-	var imgsrc = 'data:' + imgpkt.contentType + ';base64,' + b64raw;
-	$('#newImg').attr('src', imgsrc);
-  $('#countImg').html(1 + parseInt($('#countImg').html()));
-})
-
-socket.on('countDb', function(count) {
-  $('#countData').html(count);
-});
-
-socket.on('countImg', function(count) {
-  $('#countImg').html(count);
-});
-
-socket.on('historyPkt', function(pkt) {  
-  var str = pkt.date + '<br>' + pkt.noise + '<br>' + pkt.temparature + '<br>' + pkt.humidity + '<br>' + pkt.lat + '<br>' + pkt.lng;
-  $('#newData').html(str);
-  console.log("History data got: " + str);
-});
-
-// socket.on('lastImg', function (img) {
-//   var b64raw = img.raw;
-//   var imgsrc = 'data:' + img.contentType + ';base64,' + b64raw;
-//   $('#newImg').attr('src', imgsrc);
-//   console.log("Last image got");
-// })
--- roofserver.js
+++ roofserver.js
@@ -3,21 +3,40 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5001;
 
 server.listen(port, function() {
   console.log("Express server listening on port %d", server.address().port);
 });
 
+var reqHandlers = {
+  monitorHandler: function(socket) {
+    var monitorAgent = { 
+      init: function() {
+        console.log('A monitor connected');
+        var intervalId = this.updateClock();
+        socket.on('disconnect', function() {
+          console.log('monitor disconnected'); 
+          clearInterval(intervalId);
+        });
+      },
+      updateClock: function() {
+        socket.emit('date', {'date': new Date()});
+        return setInterval(function() { 
+          socket.emit('date', {'date': new Date()});
+          // console.log("My port is " + process.env.PORT);
+        }, 1000);
+      },
+    };
+    monitorAgent.init(); 
+  }
+};
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 io.on('connection', function (socket) {  // connection setup for monitor.html
-	console.log("A user connencted");
-	socket.emit('msg1', 'welcome');
-	socket.on('disconnect', function() {
-		console.log("A user disconnect");
-	})
+	reqHandlers.monitorHandler(socket);
 });
 
 

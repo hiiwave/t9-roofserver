8fc2805269402852ae03db0b84b85672a4370783
WaveDorm <hiiwave@gmail.com>
2015-06-18 17:05:16 +0800

    .

--- public/monitor.js
+++ public/monitor.js
@@ -11,5 +11,6 @@ socket.on('date', function(data) {
 socket.on('newPkt', function(pkt) {
   var str = pkt.humi
   $('#humidity').html(str);
+  console.log("pkt = " + pkt + ", pkt.humi = " + pkt.humi);
   $('#countData').html(1 + parseInt($('#countData').html()));
 });

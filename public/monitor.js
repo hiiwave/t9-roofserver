15f95a6cac7c7b0126b5201ac48fdb59fa3db908
pojung <wesley041356@gmail.com>
2015-06-20 23:09:59 +0800

    fix bug

--- public/monitor.js
+++ public/monitor.js
@@ -1,15 +1,54 @@
 var socket = io.connect();
+var drawer, bindSocketEvents;
 
-socket.on('connect', function () {
-  console.log('User connected!');
+
+$(document).ready(function() {
+  bindSocketEvents();
+  drawer.init();
 });
 
-socket.on('date', function(data) {
-  $('#date').text(data.date);
-});
+bindSocketEvents = function() {
+	socket.on('connect', function () {
+	  console.log('User connected!');
+	});
 
-socket.on('newPkt', function(pkt) {
-  var str = pkt.humi;
-  $('#humidity').html(str);
-  $('#countData').html(1 + parseInt($('#countData').html()));
-});
+	socket.on('date', function(data) {
+	  $('#date').text(data.date);
+	});
+
+	socket.on('newPkt', function(pkt) {
+	  var str = pkt.humi;
+	  $('#humidity').html(str);
+	  $('#countData').html(1 + parseInt($('#countData').html()));
+
+	  drawer.update(str);
+	});
+};
+
+drawer = {
+	init: function(pkt) {
+        var div1=d3.select(document.getElementById('div1'));
+ 
+		var rp1 = radialProgress(document.getElementById('div1'))
+                .label("HUMIDITY")
+                .onClick(onClick1)
+                .diameter(150)
+                .value(pkt)
+                .render();
+    },
+    update: function(pkt) {
+
+
+		radialProgress(document.getElementById('div1'))
+		  .diameter(150)
+		  .value(pkt)
+		  .render();
+	},
+
+	onClick1: function() {
+	    deselect();
+	    div1.attr("class","selectedRadial");
+	},
+
+    
+}
